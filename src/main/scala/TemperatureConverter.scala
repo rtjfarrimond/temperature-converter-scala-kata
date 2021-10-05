24574c2293a68552adaf7ba8d77432800b581ff2
Robert Farrimond <rtjfarrimond@gmail.com>
2021-10-05 09:26:45 +0100

    Implement solution to first exercise

--- src/main/scala/TemperatureConverter.scala
+++ src/main/scala/TemperatureConverter.scala
@@ -1,10 +1,20 @@
-trait Temperature
+import scala.util.Try
+import scala.util.Success
+import scala.util.Failure
+
+sealed trait Temperature {
+  def value: Double
+}
+case class CelsiusTemperature(value: Double) extends Temperature
+case class FahrenheitTemperature(value: Double) extends Temperature
 
 object TemperatureConverter:
 
-  def celsiusToFahrenheit(tempCelsius: Double): Double = ???
+  def celsiusToFahrenheit(tempCelsius: Double): Double =
+    tempCelsius * 9.0 / 5.0 + 32.0
 
-  def fahrenheitToCelsius(tempFahrenheit: Double): Double  = ???
+  def fahrenheitToCelsius(tempFahrenheit: Double): Double  =
+    (tempFahrenheit - 32.0) * 5.0 / 9.0
 
   /**
    * Strings are expected in the format '42C' or '42F'.
@@ -12,6 +22,38 @@ object TemperatureConverter:
    * See the tests for a more formal definition of the expected
    * behaviour.
    */
-  def parse(input: String): Either[String, Temperature] = ???
+  def parse(input: String): Either[String, Temperature] = {
+    val inputLower = input.toLowerCase
+    if (inputLower == "") Left("Input was the empty String")
+    else {
+      val (temp, unit) = inputLower.splitAt(input.length - 1)
+      (temp, unit) match {
+        case ("", _) =>
+          Left(s"No temperature was provided in input $inputLower")
+        case (t, u) =>
+          Try(u.toInt) match {
+            case Success(_) =>
+              Left("No unit was provided")
+            case Failure(_) =>
+              Try(t.toDouble) match {
+                case Failure(error) =>
+                  Left(s"Failed to parse temperature: $error")
+                case Success(tempDouble) =>
+                  u match {
+                    case "c" => Right(CelsiusTemperature(tempDouble))
+                    case "f" => Right(FahrenheitTemperature(tempDouble))
+                    case _ => Left(s"Unit must be one of c or f, but was $u")
+                  }
+              }
+          }
+      }
+    }
+  }
 
-  def convert(temperature: Temperature): Temperature = ???
+  def convert(temperature: Temperature): Temperature =
+    temperature match {
+      case CelsiusTemperature(v) =>
+        FahrenheitTemperature(celsiusToFahrenheit(v))
+      case FahrenheitTemperature(v) =>
+        CelsiusTemperature(fahrenheitToCelsius(v))
+    }
--- src/test/scala/TemperatureConverterSpec.scala
+++ src/test/scala/TemperatureConverterSpec.scala
@@ -38,48 +38,48 @@ class TempertatureConverterSpec extends AnyFlatSpec with Matchers:
   }
 
   "parse" must "return Left if input is the empty String" in {
-    parse("") mustBe Left(???)
+    parse("") mustBe Left("Input was the empty String")
   }
 
   it must "return Left if no unit is provided" in {
-    parse("42") mustBe Left(???)
+    parse("42") mustBe Left("No unit was provided")
   }
 
   it must "return Left if unit is not c or f" in {
     val unit = 'g'
-    parse("42g") mustBe Left(???)
+    parse(s"42$unit") mustBe Left(s"Unit must be one of c or f, but was $unit")
   }
 
   it must "return Left if no temperature is provided" in {
     val input = "c"
-    parse(input) mustBe Left(???)
+    parse(input) mustBe Left(s"No temperature was provided in input $input")
   }
 
   it must "return Left if temperature is not parsable number" in {
     val input = "fooc"
-    parse(input) mustBe Left(???)
+    parse(input) mustBe Left("Failed to parse temperature: java.lang.NumberFormatException: For input string: \"foo\"")
   }
 
   it must "return Right with Temperature in Celsius when input has lowercase c" in {
-    parse("42c") mustBe Right(???)
+    parse("42c") mustBe Right(CelsiusTemperature(42.0))
   }
 
   it must "return Right with Temperature in Celsius when input has uppercase C" in {
-    parse("42C") mustBe Right(???)
+    parse("42C") mustBe Right(CelsiusTemperature(42.0))
   }
 
   it must "return Right with Temperature in Fahrenheit when input has lowercase f" in {
-    parse("42f") mustBe Right(???)
+    parse("42f") mustBe Right(FahrenheitTemperature(42.0))
   }
 
   it must "return Right with Temperature in Fahrenheit when input has uppercase F" in {
-    parse("42F") mustBe Right(???)
+    parse("42F") mustBe Right(FahrenheitTemperature(42.0))
   }
 
   "convert" must "convert Temperature in Celsius to correct Temperature in Fahrenheit" in {
-    convert(???) mustBe ???
+    convert(CelsiusTemperature(-40)) mustBe FahrenheitTemperature(-40)
   }
 
   it must "convert Temperature in Fahrenheit to correct Temperature in Celcius" in {
-    convert(???) mustBe ???
+    convert(FahrenheitTemperature(-40)) mustBe CelsiusTemperature(-40)
   }
